2b4989761ce2d8ddb8a196f12501b9b959eaf489
Nob Fox <thenobfox@gmail.com>
2024-10-03 07:35:21 +0100

    Update cryptogames.cs
    Changed MATIC to POL for CryptoGames

--- DiceBot/cryptogames.cs
+++ DiceBot/cryptogames.cs
@@ -21,7 +21,7 @@ namespace DiceBot
         HttpClient Client;// = new HttpClient { BaseAddress = new Uri("https://api.primedice.com/api/") };
         HttpClientHandler ClientHandlr;
         
-        public static string[] sCurrencies = new string[] { "BCH", "BTC", "DASH", "DOGE", "ETC", "ETH", "GAS", "LTC", "MATIC", "SHIB", "SOL", "PLAY", "USDT", "XMR" };
+        public static string[] sCurrencies = new string[] { "BCH", "BTC", "DASH", "DOGE", "ETC", "ETH", "GAS", "LTC", "POL", "SHIB", "SOL", "PLAY", "USDT", "XMR" };
         public cryptogames(cDiceBot Parent)
         {
             _PasswordText = "API Key: ";
